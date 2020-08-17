6032ef9e48d75cb8e99ec6df8def1385cd706b37
franklinv <franklin.vincent@gmail.com>
2020-08-18 02:26:32 +1000

    added config file

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {
+development: {
+    //url to be used in link generation
+    //mongodb connection settings
+    database: {
+        username:   'dbUser',
+        password:   'dbUser'
+  
+    }
+}
+// other environments
+
+};
+module.exports = config;
